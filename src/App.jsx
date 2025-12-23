b203c2305ee497cae585a2970015be40eef5bad0
Shrest2k5 <shrestshaw2k5@gmail.com>
2025-12-23 23:59:30 +0530

    Home page and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react'
 import Navbar from './components/Navbar'
-import { useLocation } from 'react-router-dom';
+import { Route, Routes,useLocation } from 'react-router-dom';
+import Home from './pages/Home';
+import CarDetails from './pages/CarDetails';
+import Cars from './pages/Cars';
+import MyBookings from './pages/MyBookings';
+import Footer from './components/Footer';
 
 const App = () => {
 
@@ -11,6 +16,17 @@ const App = () => {
     <>
 
       {!isOwnerPath && <Navbar setShowLogin={setShowLogin}/>}
+
+
+      <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/car-details/:id' element={<CarDetails/>}/>
+      <Route path='/cars' element={<Cars/>}/>
+      <Route path='/my-bookings' element={<MyBookings/>}/>
+      </Routes>
+
+      {!isOwnerPath && <Footer/>}
+      
     </>
   )
 }
